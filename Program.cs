06f9ffa940b77cc05c2f1e4f7f6ec0dd14e4e83e
Natalya-Laz <110369574+Natalya-Laz@users.noreply.github.com>
2022-09-16 22:43:14 +0500

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// в трёхзначном числе показывает вторую цифру этого числа
-int num = new Random ().Next(100,1000);
-int a = num/10;
-int b = a%10;
-
-Console.WriteLine($"Выбранное число:{num}");
-
-Console.WriteLine($"Второе число {a%10}");
